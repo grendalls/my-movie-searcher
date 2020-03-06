ca999a7c49403d4a3d4236672a555ef6f505831b
Dmitriy Denisov <aldzir3@gmail.com>
2020-03-06 14:28:53 +0300

    #3: Add redux-thunk, test async actions

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-
 import Header from '../header';
+import { connect } from 'react-redux';
 
-const App = () => {
+const asyncAction = () => async dispatch => {
+  dispatch({
+    type: 'test',
+    payload: 'sadad'
+  });
+  const a = () => setTimeout(() => dispatch({
+    type: 'async_test',
+    payload: 'asdsda'
+  }), 2000);
+  const res = await Promise.resolve().then(a);
+
+}
+
+const App = (props) => {
+  props.asyncAction();
   return (
     <div>
       <Header />
-
     </div>
   );
 };
 
-export default App;
+export default connect(null, {
+  asyncAction
+})(App);
